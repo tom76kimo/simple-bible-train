03de45e12e284f0d195b412d224ed1d8dfd18f73
Tom Wu <tom76kimo@yahoo-inc.com>
2018-03-26 23:57:12 +0800

    Add bulma and simple navbabr

--- src/App.js
+++ src/App.js
@@ -6,10 +6,25 @@ class App extends Component {
     render() {
         return (
             <div className="App">
-                <header className="App-header">
-                    <img src={logo} className="App-logo" alt="logo" />
-                    <h1 className="App-title">Welcome to React, aws</h1>
-                </header>
+                <div className="container">
+                    <nav className="navbar" role="navigation" aria-label="main navigation">
+                        <div className="navbar-brand">
+                            <a className="navbar-item" href="/">
+                                Bible Train
+                            </a>
+                            <div className="navbar-burger">
+                                <span />
+                                <span />
+                                <span />
+                            </div>
+                        </div>
+                        {/* <div className="navbar-menu">
+                            <div className="navbar-start">
+                                <div className="navbar-item">adsf</div>
+                            </div>
+                        </div> */}
+                    </nav>
+                </div>
                 <p className="App-intro">
                     To get started, edit <code>src/App.js</code> and save to reload.
                 </p>
